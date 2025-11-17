6a2660a2515926090b90b38606adea5c22aba462
Paul Ochieng <akelopauljudeochieng@gmail.com>
2025-11-17 20:16:43 +0300

    feat: add ContactPage route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { ProjectsPage} from "./pages/ProjectsPage.jsx";
 import { AboutPage } from './pages/AboutPage.jsx'
+import { ContactPage } from "./pages/ContactPage.jsx";
 import '../styles/main.css'
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route index element={<HomePage />} />
         <Route path="projects" element={<ProjectsPage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactPage />} />
     </Routes>
   )
 }
